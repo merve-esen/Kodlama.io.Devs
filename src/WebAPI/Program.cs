3b67339a34c8cb4aa38f21d64e03f24a410c37cb
Merve Esen <merveesen834@gmail.com>
2022-09-02 14:42:53 +0300

    Update Program.cs

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using Application;
+using Core.CrossCuttingConcerns.Exceptions;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,6 +25,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+if (app.Environment.IsProduction())
+    app.ConfigureCustomExceptionMiddleware();
 
 app.UseAuthorization();
 
