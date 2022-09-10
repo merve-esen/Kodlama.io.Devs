7f0f3accd4e975396c1fa126f76b17b07c9f6dbb
merve-esen <merveesen834@gmail.com>
2022-09-10 21:38:26 +0300

    Day 4 - Assignment 1

--- src/Application/ApplicationServiceRegistration.cs
+++ src/Application/ApplicationServiceRegistration.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Features.Auth.Rules;
 using Application.Features.ProgrammingLanguages.Rules;
 using Application.Features.Technologies.Rules;
+using Core.Application.Pipelines.Authorization;
 using Core.Application.Pipelines.Validation;
 using FluentValidation;
 using MediatR;
@@ -22,7 +23,7 @@ namespace Application
             services.AddScoped<AuthBusinessRules>();
 
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehavior<,>));
             //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CachingBehavior<,>));
             //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CacheRemovingBehavior<,>));
             //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
