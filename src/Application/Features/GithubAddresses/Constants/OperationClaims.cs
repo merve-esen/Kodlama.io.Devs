1924a114fd0eeca9a28624607fbde2223d64694f
Merve Esen <merveesen834@gmail.com>
2022-10-25 16:53:22 +0300

    Create OperationClaims.cs

---
+++ src/Application/Features/GithubAddresses/Constants/OperationClaims.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Features.GithubAddresses.Constants
+{
+    public static class OperationClaims
+    {
+        public const string GithubAddressAdd = "githubaddress.add";
+        public const string GithubAddressUpdate = "githubaddress.update";
+        public const string GithubAddressDelete = "githubaddress.delete";
+    }
+}
