ed21bb32259d9f7bfec0d7bbe49dd0638ddec4c6
Merve Esen <merveesen834@gmail.com>
2022-10-25 16:53:17 +0300

    Create OperationClaimRoles.cs

---
+++ src/Application/Features/OperationClaims/Constants/OperationClaimRoles.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.Features.OperationClaims.Constants
+{
+    public class OperationClaimRoles
+    {
+        public const string OperationClaimAdmin = "OperationClaim.Admin";
+
+        public const string OperationClaimRead = "OperationClaim.Read";
+        public const string OperationClaimWrite = "OperationClaim.Write";
+
+        public const string OperationClaimGetById = "OperationClaim.GetById";
+        public const string OperationClaimGetList = "OperationClaim.GetList";
+
+        public const string OperationClaimCreate = "OperationClaim.Create";
+        public const string OperationClaimDelete = "OperationClaim.Delete";
+        public const string OperationClaimUpdate = "OperationClaim.Update";
+    }
+}
