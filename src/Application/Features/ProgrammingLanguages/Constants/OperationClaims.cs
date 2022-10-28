4afafc507073b298c81b4c23418accfef3971038
merve-esen <merveesen834@gmail.com>
2022-10-28 20:48:12 +0300

    Create OperationClaims.cs

---
+++ src/Application/Features/ProgrammingLanguages/Constants/OperationClaims.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.Features.ProgrammingLanguages.Constants
+{
+    public class OperationClaim
+    {
+        public const string ProgrammingLanguageAdmin = "ProgrammingLanguage.Admin";
+
+        public const string ProgrammingLanguageRead = "ProgrammingLanguage.Read";
+        public const string ProgrammingLanguageWrite = "ProgrammingLanguage.Write";
+
+        public const string ProgrammingLanguageGetById = "ProgrammingLanguage.GetById";
+        public const string ProgrammingLanguageGetList = "ProgrammingLanguage.GetList";
+
+        public const string ProgrammingLanguageCreate = "ProgrammingLanguage.Create";
+        public const string ProgrammingLanguageDelete = "ProgrammingLanguage.Delete";
+        public const string ProgrammingLanguageUpdate = "ProgrammingLanguage.Update";
+    }
+}
