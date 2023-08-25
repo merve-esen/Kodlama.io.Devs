b863e2538e0356e2affc3130918b666f5547313f
merve-esen <merveesen834@gmail.com>
2023-08-25 22:10:10 +0300

    Unit test configurations

---
+++ tests/Application.Tests/DependencyResolvers/UsersTestServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Tests.DependencyResolvers
+{
+    public static class UsersTestServiceRegistration
+    {
+        public static void AddUsersServices(this IServiceCollection services)
+        {
+
+        }
+    }
+}
