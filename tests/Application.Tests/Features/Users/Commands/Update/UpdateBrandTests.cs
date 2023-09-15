a839a0463f1fbb9a8a70d8ebb70aa529559e7dcd
merve-esen <merveesen834@gmail.com>
2023-09-15 21:00:50 +0300

    Create UpdateBrandTests.cs

---
+++ tests/Application.Tests/Features/Users/Commands/Update/UpdateBrandTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Tests.Features.Users.Commands.Update
+{
+    internal class UpdateBrandTests
+    {
+    }
+}
