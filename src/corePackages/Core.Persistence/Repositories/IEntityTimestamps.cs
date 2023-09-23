574c7e3817044e4a81419da8155a1c4a92ef392f
merve-esen <merveesen834@gmail.com>
2023-09-23 21:13:14 +0300

    Create IEntityTimestamps.cs

---
+++ src/corePackages/Core.Persistence/Repositories/IEntityTimestamps.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Persistence.Repositories
+{
+    public interface IEntityTimestamps
+    {
+        DateTime CreatedDate { get; set; }
+        DateTime? UpdatedDate { get; set; }
+        DateTime? DeletedDate { get; set; }
+    }
+}
