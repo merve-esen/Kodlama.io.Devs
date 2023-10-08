236feb914112edefae89cbb9ce40f370cc3fa2a9
merve-esen <merveesen834@gmail.com>
2023-10-08 20:41:59 +0300

    Update Entity.cs

--- src/corePackages/Core.Persistence/Repositories/Entity.cs
+++ src/corePackages/Core.Persistence/Repositories/Entity.cs
@@ -1,14 +1,18 @@
 ﻿namespace Core.Persistence.Repositories;
 
-public class Entity
+public class Entity<TId> : IEntityTimestamps
 {
-    public int Id { get; set; }
+    public TId Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
 
     public Entity()
     {
+        Id = default!;
     }
 
-    public Entity(int id) : this()
+    public Entity(TId id)
     {
         Id = id;
     }
