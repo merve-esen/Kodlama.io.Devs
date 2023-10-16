37ef5359e961b95359c05378d4595174cc672827
merve-esen <merveesen834@gmail.com>
2023-10-16 20:44:39 +0300

    Update IRepository.cs

--- src/corePackages/Core.Persistence/Repositories/IRepository.cs
+++ src/corePackages/Core.Persistence/Repositories/IRepository.cs
@@ -1,24 +1,45 @@
-﻿using System.Linq.Expressions;
+﻿using Core.Persistence.Dynamic;
 using Core.Persistence.Paging;
 using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
 
 namespace Core.Persistence.Repositories;
 
-public interface IRepository<T> : IQuery<T> where T : Entity
+public interface IRepository<TEntity, TEntityId> : IQuery<TEntity>
+    where TEntity : Entity<TEntityId>
 {
-    T Get(Expression<Func<T, bool>> predicate);
+    TEntity? Get(
+        Expression<Func<TEntity, bool>> predicate,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        bool withDeleted = false,
+        bool enableTracking = true
+    );
 
-    IPaginate<T> GetList(Expression<Func<T, bool>>? predicate = null,
-                         Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
-                         Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
-                         int index = 0, int size = 10,
-                         bool enableTracking = true);
+    IPaginate<TEntity> GetList(
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        int index = 0,
+        int size = 10,
+        bool withDeleted = false,
+        bool enableTracking = true
+    );
 
-    IPaginate<T> GetListByDynamic(Dynamic.Dynamic dynamic,
-                                  Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
-                                  int index = 0, int size = 10, bool enableTracking = true);
+    IPaginate<TEntity> GetListByDynamic(
+        DynamicQuery dynamic,
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        int index = 0,
+        int size = 10,
+        bool withDeleted = false,
+        bool enableTracking = true
+    );
 
-    T Add(T entity);
-    T Update(T entity);
-    T Delete(T entity);
+    bool Any(Expression<Func<TEntity, bool>>? predicate = null, bool withDeleted = false, bool enableTracking = true);
+    TEntity Add(TEntity entity);
+    ICollection<TEntity> AddRange(ICollection<TEntity> entities);
+    TEntity Update(TEntity entity);
+    ICollection<TEntity> UpdateRange(ICollection<TEntity> entities);
+    TEntity Delete(TEntity entity, bool permanent = false);
+    ICollection<TEntity> DeleteRange(ICollection<TEntity> entity, bool permanent = false);
 }
