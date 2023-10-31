57c821683786c05d04dce88513aebda5b2cc649f
merve-esen <merveesen834@gmail.com>
2023-10-31 21:23:16 +0300

    Update User.cs

--- src/corePackages/Core.Security/Entities/User.cs
+++ src/corePackages/Core.Security/Entities/User.cs
@@ -13,19 +13,51 @@ public class User : Entity<int>
     public bool Status { get; set; }
     public AuthenticatorType AuthenticatorType { get; set; }
 
-    public virtual ICollection<UserOperationClaim> UserOperationClaims { get; set; }
-    public virtual ICollection<RefreshToken> RefreshTokens { get; set; }
+    public virtual ICollection<UserOperationClaim> UserOperationClaims { get; set; } = null!;
+    public virtual ICollection<RefreshToken> RefreshTokens { get; set; } = null!;
+    public virtual ICollection<EmailAuthenticator> EmailAuthenticators { get; set; } = null!;
+    public virtual ICollection<OtpAuthenticator> OtpAuthenticators { get; set; } = null!;
 
     public User()
     {
-        UserOperationClaims = new HashSet<UserOperationClaim>();
-        RefreshTokens = new HashSet<RefreshToken>();
+        FirstName = string.Empty;
+        LastName = string.Empty;
+        Email = string.Empty;
+        PasswordHash = Array.Empty<byte>();
+        PasswordSalt = Array.Empty<byte>();
     }
 
-    public User(int id, string firstName, string lastName, string email, byte[] passwordSalt, byte[] passwordHash,
-                bool status, AuthenticatorType authenticatorType) : this()
+    public User(
+        string firstName,
+        string lastName,
+        string email,
+        byte[] passwordSalt,
+        byte[] passwordHash,
+        bool status,
+        AuthenticatorType authenticatorType
+    )
     {
-        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        PasswordSalt = passwordSalt;
+        PasswordHash = passwordHash;
+        Status = status;
+        AuthenticatorType = authenticatorType;
+    }
+
+    public User(
+        int id,
+        string firstName,
+        string lastName,
+        string email,
+        byte[] passwordSalt,
+        byte[] passwordHash,
+        bool status,
+        AuthenticatorType authenticatorType
+    )
+        : base(id)
+    {
         FirstName = firstName;
         LastName = lastName;
         Email = email;
