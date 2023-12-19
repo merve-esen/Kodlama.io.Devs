216fa96118dab4b99e9550d6b8691a531bdcb50c
merve-esen <merveesen834@gmail.com>
2023-12-19 19:41:22 +0300

    Update GithubAddressRepository.cs

--- src/Persistence/Repositories/GithubAddressRepository.cs
+++ src/Persistence/Repositories/GithubAddressRepository.cs
@@ -5,7 +5,7 @@ using Persistence.Contexts;
 
 namespace Persistence.Repositories;
 
-public class GithubAddressRepository : EfRepositoryBase<GithubAddress, BaseDbContext>, IGithubAddressRepository
+public class GithubAddressRepository : EfRepositoryBase<GithubAddress, int, BaseDbContext>, IGithubAddressRepository
 {
     public GithubAddressRepository(BaseDbContext context) : base(context)
     {
