80b1b68d77b13123a2bfed10949784b1e3937c48
merve-esen <merveesen834@gmail.com>
2023-12-21 20:24:54 +0300

    Update TechnologyRepository.cs

--- src/Persistence/Repositories/TechnologyRepository.cs
+++ src/Persistence/Repositories/TechnologyRepository.cs
@@ -5,7 +5,7 @@ using Persistence.Contexts;
 
 namespace Persistence.Repositories;
 
-public class TechnologyRepository : EfRepositoryBase<Technology, BaseDbContext>, ITechnologyRepository
+public class TechnologyRepository : EfRepositoryBase<Technology, int, BaseDbContext>, ITechnologyRepository
 {
     public TechnologyRepository(BaseDbContext context) : base(context)
     {
