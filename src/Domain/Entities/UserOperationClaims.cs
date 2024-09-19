75554ea79fc1a1cb55c2b9439d7113bdca21cea3
merve-esen <merveesen834@gmail.com>
2024-09-19 22:30:50 +0300

    Create UserOperationClaims.cs

---
+++ src/Domain/Entities/UserOperationClaims.cs
@@ -0,0 +1,8 @@
+﻿using Core.Security.Entities;
+namespace Domain.Entities;
+
+public class UserOperationClaim : Core.Security.Entities.UserOperationClaim<Guid, Guid, int>
+{
+    public virtual User User { get; set; } = default!;
+    public virtual OperationClaim OperationClaim { get; set; } = default!;
+}
