9424998ddf5f891b93f2385aecaac9546304a072
merve-esen <merveesen834@gmail.com>
2024-09-22 15:15:02 +0300

    Update UserOperationClaimRepository.cs

--- src/Persistence/Repositories/UserOperationClaimRepository.cs
+++ src/Persistence/Repositories/UserOperationClaimRepository.cs
@@ -1,13 +1,24 @@
 ﻿using Application.Services.Repositories;
 using Core.Persistence.Repositories;
 using Core.Security.Entities;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using Persistence.Contexts;
 
 namespace Persistence.Repositories;
 
-public class UserOperationClaimRepository : EfRepositoryBase<UserOperationClaim, int, BaseDbContext>, IUserOperationClaimRepository
+public class UserOperationClaimRepository : EfRepositoryBase<UserOperationClaim, Guid, BaseDbContext>, IUserOperationClaimRepository
 {
-    public UserOperationClaimRepository(BaseDbContext context) : base(context)
+    public UserOperationClaimRepository(BaseDbContext context) : base(context) { }
+
+    public async Task<IList<OperationClaim>> GetOperationClaimsByUserIdAsync(Guid userId)
     {
+        List<OperationClaim> operationClaims = await Query()
+            .AsNoTracking()
+            .Where(p => p.UserId.Equals(userId))
+            .Select(p => new OperationClaim { Id = p.OperationClaimId, Name = p.OperationClaim.Name })
+            .ToListAsync();
+        return operationClaims;
     }
 }
+
