06bd1f40317ee282c68013c7cb8759d6d5922241
merve-esen <merveesen834@gmail.com>
2024-09-24 20:43:12 +0300

    Update OperationClaim.cs

--- src/corePackages/Core.Security/Entities/OperationClaim.cs
+++ src/corePackages/Core.Security/Entities/OperationClaim.cs
@@ -2,15 +2,22 @@
 
 namespace Core.Security.Entities;
 
-public class OperationClaim : Entity<int>
+public class OperationClaim<TId> : Entity<TId>
 {
     public string Name { get; set; }
 
     public OperationClaim()
     {
+        Name = string.Empty;
     }
 
-    public OperationClaim(int id, string name) : base(id)
+    public OperationClaim(string name)
+    {
+        Name = name;
+    }
+
+    public OperationClaim(TId id, string name)
+        : base(id)
     {
         Name = name;
     }
