c63b67e4df41a4f73d9ff96680f6695ecefbf35d
merve-esen <merveesen834@gmail.com>
2024-10-01 20:52:28 +0300

    Update RefreshToken.cs

--- src/corePackages/Core.Security/Entities/RefreshToken.cs
+++ src/corePackages/Core.Security/Entities/RefreshToken.cs
@@ -2,39 +2,38 @@
 
 namespace Core.Security.Entities;
 
-public class RefreshToken : Entity<int>
+public class RefreshToken<TId, TUserId> : Entity<TId>
 {
-    public int UserId { get; set; }
+    public TUserId UserId { get; set; }
     public string Token { get; set; }
-    public DateTime Expires { get; set; }
-    public DateTime Created { get; set; }
+    public DateTime ExpirationDate { get; set; }
     public string CreatedByIp { get; set; }
-    public DateTime? Revoked { get; set; }
+    public DateTime? RevokedDate { get; set; }
     public string? RevokedByIp { get; set; }
     public string? ReplacedByToken { get; set; }
-
     public string? ReasonRevoked { get; set; }
-    //public bool IsExpired => DateTime.UtcNow >= Expires;
-    //public bool IsRevoked => Revoked != null;
-    //public bool IsActive => !IsRevoked && !IsExpired;
-
-    public virtual User User { get; set; }
 
     public RefreshToken()
     {
+        UserId = default!;
+        Token = string.Empty;
+        CreatedByIp = string.Empty;
     }
 
-    public RefreshToken(int id, string token, DateTime expires, DateTime created, string createdByIp, DateTime? revoked,
-                        string revokedByIp, string replacedByToken, string reasonRevoked)
+    public RefreshToken(TUserId userId, string token, DateTime expirationDate, string createdByIp)
     {
-        Id = id;
+        UserId = userId;
         Token = token;
-        Expires = expires;
-        Created = created;
+        ExpirationDate = expirationDate;
         CreatedByIp = createdByIp;
-        Revoked = revoked;
-        RevokedByIp = revokedByIp;
-        ReplacedByToken = replacedByToken;
-        ReasonRevoked = reasonRevoked;
+    }
+
+    public RefreshToken(TId id, TUserId userId, string token, DateTime expirationDate, string createdByIp)
+        : base(id)
+    {
+        UserId = userId;
+        Token = token;
+        ExpirationDate = expirationDate;
+        CreatedByIp = createdByIp;
     }
 }
