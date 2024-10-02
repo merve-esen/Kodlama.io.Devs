e0b924f00c0263b265b0da981282fd04eb8d0a5e
merve-esen <merveesen834@gmail.com>
2024-10-02 20:34:07 +0300

    Update AccessToken.cs

--- src/corePackages/Core.Security/JWT/AccessToken.cs
+++ src/corePackages/Core.Security/JWT/AccessToken.cs
@@ -3,5 +3,16 @@
 public class AccessToken
 {
     public string Token { get; set; }
-    public DateTime Expiration { get; set; }
+    public DateTime ExpirationDate { get; set; }
+
+    public AccessToken()
+    {
+        Token = string.Empty;
+    }
+
+    public AccessToken(string token, DateTime expirationDate)
+    {
+        Token = token;
+        ExpirationDate = expirationDate;
+    }
 }
