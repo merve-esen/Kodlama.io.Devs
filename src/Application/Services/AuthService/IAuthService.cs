46346e3fa025b12e9b3b0988272b9a3763007952
merve-esen <merveesen834@gmail.com>
2024-10-14 21:53:52 +0300

    Update IAuthService.cs

--- src/Application/Services/AuthService/IAuthService.cs
+++ src/Application/Services/AuthService/IAuthService.cs
@@ -9,4 +9,6 @@ public interface IAuthService
     public Task<RefreshToken> CreateRefreshToken(User user, string ipAddress);
     public Task<RefreshToken?> GetRefreshTokenByToken(string refreshToken);
     public Task<RefreshToken> AddRefreshToken(RefreshToken refreshToken);
+    public Task DeleteOldRefreshTokens(Guid userId);
+    public Task RevokeDescendantRefreshTokens(RefreshToken refreshToken, string ipAddress, string reason);
 }
