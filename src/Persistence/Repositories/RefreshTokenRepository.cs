ffe7d9b79179c6856114ad1fa743526cf0cc239c
merve-esen <merveesen834@gmail.com>
2024-10-21 20:23:35 +0300

    Update RefreshTokenRepository.cs

--- src/Persistence/Repositories/RefreshTokenRepository.cs
+++ src/Persistence/Repositories/RefreshTokenRepository.cs
@@ -1,11 +1,12 @@
 ﻿using Application.Services.Repositories;
 using Core.Persistence.Repositories;
 using Core.Security.Entities;
+using Domain.Entities;
 using Persistence.Contexts;
 
 namespace Persistence.Repositories;
 
-public class RefreshTokenRepository : EfRepositoryBase<RefreshToken, int, BaseDbContext>, IRefreshTokenRepository
+public class RefreshTokenRepository : EfRepositoryBase<RefreshToken, Guid, BaseDbContext>, IRefreshTokenRepository
 {
     public RefreshTokenRepository(BaseDbContext context) : base(context)
     {
