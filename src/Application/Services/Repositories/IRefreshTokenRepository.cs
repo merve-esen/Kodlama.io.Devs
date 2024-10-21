dbc649132a504ca4001406011b4f12d8f46e4b75
merve-esen <merveesen834@gmail.com>
2024-10-21 19:12:11 +0300

    Update IRefreshTokenRepository.cs

--- src/Application/Services/Repositories/IRefreshTokenRepository.cs
+++ src/Application/Services/Repositories/IRefreshTokenRepository.cs
@@ -5,4 +5,5 @@ namespace Application.Services.Repositories;
 
 public interface IRefreshTokenRepository : IAsyncRepository<RefreshToken, Guid>, IRepository<RefreshToken, Guid>
 {
+    Task<List<RefreshToken>> GetOldRefreshTokensAsync(Guid userId, int refreshTokenTTL);
 }
