c13f6348fda945c9982569f0e9b5c64d4b0e9dfd
merve-esen <merveesen834@gmail.com>
2024-10-24 20:08:11 +0300

    Update RefreshTokenRepository.cs

--- src/Persistence/Repositories/RefreshTokenRepository.cs
+++ src/Persistence/Repositories/RefreshTokenRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Services.Repositories;
 using Core.Persistence.Repositories;
 using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using Persistence.Contexts;
 
 namespace Persistence.Repositories;
@@ -9,5 +10,20 @@ public class RefreshTokenRepository : EfRepositoryBase<RefreshToken, Guid, BaseD
 {
     public RefreshTokenRepository(BaseDbContext context) : base(context)
     {
+    }
+
+    public async Task<List<RefreshToken>> GetOldRefreshTokensAsync(Guid userId, int refreshTokenTtl)
+    {
+        List<RefreshToken> tokens = await Query()
+            .AsNoTracking()
+            .Where(r =>
+                r.UserId == userId
+                && r.RevokedDate == null
+                && r.ExpirationDate >= DateTime.UtcNow
+                && r.CreatedDate.AddDays(refreshTokenTtl) <= DateTime.UtcNow
+            )
+            .ToListAsync();
+
+        return tokens;
     }
 }
