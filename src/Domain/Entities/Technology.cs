78e7a5ced692825315891b9d0ae86ef6d1658f47
merve-esen <merveesen834@gmail.com>
2024-11-14 20:28:20 +0300

    Update Technology.cs

--- src/Domain/Entities/Technology.cs
+++ src/Domain/Entities/Technology.cs
@@ -10,6 +10,7 @@ public class Technology : Entity<int>
     public virtual ProgrammingLanguage ProgrammingLanguage { get; set; }
     public Technology()
     {
+        Name = string.Empty;
     }
 
     public Technology(int id, int programmingLanguageId, string name) : this()
